25839dfaca3a4b7ebbbe7e4345615bb1b82305fc
Marisa You <marisayou2017@u.northwestern.edu>
2020-11-15 14:59:54 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let messages = [names.length];
+    for (let i = 0; i < names.length; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return messages;
+}
+
+function countDown(num) {
+    while (num > -1) {
+        console.log(num);
+        num--;
+    }
+}
